09c1975f20eda06836556c9798472b35b4018738
chat26666 <chat26666@gmail.com>
2025-03-21 13:28:10 +0900

    fix : 일정 생성시 잘못된 writerID로 생성시 예외처리

--- src/main/java/com/example/diary/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/diary/exception/GlobalExceptionHandler.java
@@ -1,4 +1,6 @@
 package com.example.diary.exception;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -6,6 +8,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import java.util.List;
 
 
@@ -20,12 +23,18 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<String> handlerEmptyResultDataAccess(EmptyResultDataAccessException ez) {
-        return new ResponseEntity<>("해당 ID로 조회되지 않습니다. 올바른 작성자 ID 및 일정 ID를 입력해주세요.", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("해당 ID로 조회되지 않습니다. 올바른 작성자 ID 및 일정 ID 를 입력해주세요.", HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handlerMethodArgumentNotValid(MethodArgumentNotValidException ez) {
         List<String> errorList = ez.getBindingResult().getFieldErrors().stream().
                 map(error -> error.getField() + ": " + error.getDefaultMessage()).toList();
         return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+        //입력값을 검증합니다
+        //만약 입력값이 빠졌거나 잘못된 값이 입력될 경우 해당 에러들을 리스트로 모아서 클라이언트에게 반환합니다
     }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handlerDataIntegrityViolation(DataIntegrityViolationException ez) {
+        return new ResponseEntity<>("등록되지 않은 사용자 ID 입니다.", HttpStatus.BAD_REQUEST);
+    } //해당 메서드는 일정 생성 시에 writer 테이블을 외래키로 참조하는 diary 테이블이 writer 테이블에 없는 키로 생성하려하면 발생하는 익셉션을 처리해줍니다
 }
