6755f6c537bfd6d3fd0d76ebb0e204c0f43ef8a3
chat26666 <chat26666@gmail.com>
2025-03-22 16:12:37 +0900

    refactor : Entity 멤버변수 수정

--- src/main/java/com/example/diary/entity/Writer.java
+++ src/main/java/com/example/diary/entity/Writer.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
+
 
 @Getter
 @Setter
@@ -16,9 +16,8 @@ public class Writer {
     private Integer writerId;
     private String name;
     private String email;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private List<Diary> diaries;    //DB스키마와 유사하게 1대 다수(N)의 관계성을 최대한 자바 코드에서도 비슷하게 그려냈습니다
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
 }
 
 
