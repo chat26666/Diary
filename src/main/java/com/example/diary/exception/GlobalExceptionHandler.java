51617317fcbb9c48b319f390d84bb00385d73dbc
chat26666 <chat26666@gmail.com>
2025-03-22 20:01:19 +0900

    fix : 페이지 및 사이즈 음수값 입력 예외처리

--- src/main/java/com/example/diary/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/diary/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,13 +26,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handlerMethodArgumentNotValid(MethodArgumentNotValidException ez) {
         List<String> errorList = ez.getBindingResult().getFieldErrors().stream().
-                map(error -> error.getField() + ": " + error.getDefaultMessage()).toList();
+                map(error -> error.getField() + " : " + error.getDefaultMessage()).toList();
         return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
-        //입력값을 검증합니다
+        //클라이언트의 입력값을 검증합니다
         //만약 입력값이 빠졌거나 잘못된 값이 입력될 경우 해당 에러들을 리스트로 반환합니다
     }
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handlerDataIntegrityViolation(DataIntegrityViolationException ez) {
         return new ResponseEntity<>("등록되지 않은 사용자 입니다.", HttpStatus.BAD_REQUEST);
-    } //해당 메서드는 일정 생성 시에 writer 테이블을 외래키로 참조하는 diary 테이블이 writer 테이블에 없는 키로 생성하려 할때 발생하는 예외를 처리해줍니다
+        //해당 메서드는 일정 생성 시에 writer 테이블을 외래키로 참조하는 diary 테이블이 writer 테이블에 없는 키로 생성하려 할때 발생하는 예외를 처리해줍니다
+    }
+    @ExceptionHandler(BadSqlGrammarException.class)
+    public ResponseEntity<String> handlerBadSqlGrammarException(BadSqlGrammarException ez) {
+        return new ResponseEntity<>("페이지 및 사이즈를 제대로 입력해주십시오", HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/example/diary/repo/JdbcTemplateDiaryRepository.java
+++ src/main/java/com/example/diary/repo/JdbcTemplateDiaryRepository.java
@@ -4,6 +4,7 @@ import com.example.diary.entity.Diary;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -61,6 +62,7 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
                         rs.getString("plan")
                 )
         );
+
         //해당 쿼리는 작성자 ID 와 이름이 동시에 만족한 행을 전부 읽어옵니다
         //두가지 조건을 반드시 만족해야 select 됩니다
         //select 시 조건에 맞지 않아 아무런 값이 조회되지 않으면 자동으로 빈 리스트를 리턴합니다
@@ -77,6 +79,7 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
                         rs.getString("plan")
                 )
         );
+
         //queryForObject는 row 가 조회되지 않을시 EmptyResultDataAccessException 예외를 발생시키며 해당 메서드는 throws 하여 해당 예외는 ExceptionHandler 에서 처리합니다
         //사용자와 일정관리 테이블을 나눴기 때문에 사용자 계정을 먼저 등록해야합니다 따라서 일정 ID 뿐만 아니라 사용자 ID도 일치해야 조회가 가능합니다(이름이 같은 사람일 경우 대비)
     }
@@ -89,6 +92,7 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
                 diary.getWriterId(),
                 diary.getDiaryId()
         );
+
         //비밀번호 값 검증은 서비스 레이어에서 실행되며 Repo에서는 순수하게 CRUD 기능에 충실하게끔 변경처리하였습니다
         //추가적으로 코드 상에서는 구현되어있지 않지만 일정이 변경이되면 작성자 테이블의 수정시간도 자동으로 변경되게끔 처리되어있습니다
         //DB 트리거의 기능을 활용하여 일정 테이블이 UPDATE 되는 시점에 작성자 테이블의 최근수정시간이 트리거로 인해 자동으로 똑같이 변경됩니다
@@ -101,6 +105,7 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
                 new Object[] { diary.getWriterId(), diary.getDiaryId() },
                 String.class
         );
+
         //해당 메서드는 해쉬된 비밀번호를 조회해서 리턴하는 메서드입니다. 비밀번호값 검증에서 이용됩니다
         //조회가 되지않으면 EmptyResultDataAccessException 예외를 던집니다
     }
@@ -113,7 +118,7 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
         );
     }
     @Override
-    public List<DiaryResponseDto> getPageDiary(Integer writerId, DiaryFindPageRequestDto dto,int page,int size) {
+    public List<DiaryResponseDto> getPageDiary(Integer writerId, DiaryFindPageRequestDto dto,int page,int size) throws BadSqlGrammarException {
         return jdbcTemplate.query(
                 "SELECT name, plan " +
                         "FROM diary " +
@@ -126,9 +131,10 @@ public class JdbcTemplateDiaryRepository implements DiaryRepository {
                         rs.getString("plan")
                 )
         );
+
         //단순한 페이지와 사이즈만 전달받기에 따로 엔티티로 받아서 넘기지않고 바로 dto를 넘겼습니다
         //페이징 기능을 쿼리에서 offset 과 limit 으로 처리하였습니다
         //limit 은 한꺼번에 읽을 페이지의 사이즈이고 page 번호는 쿼리스트링으로 넘겨진 값을 곱해서 시작 위치(offset)를 조정해주었습니다
-        //범위를 벗어나면 빈 배열을 리턴합니다
+        //범위를 벗어나면 빈 배열을 리턴하나 만약 page 와 size 가 음수값으로 올 경우 BadSqlGrammarException 가 throws 됩니다
     }
 }
